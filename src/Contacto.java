1227008f1c942b9a4b4ea5ee1c6271a822f68152
israel <israel.vergara102824@gmail.com>
2017-10-25 11:13:34 -0500

    se agrega validacion de telefono y se agregan contactos estos se imprimen

--- src/Contacto.java
+++ src/Contacto.java
@@ -18,11 +18,30 @@ public class Contacto
 
             throw new IllegalArgumentException("El nombre debe iniciar con una letra");
         }
+        dir = dir.trim();// regresa una copia y quita los espacios del inicio y al final
         if(dir.isEmpty()){
             throw new IllegalArgumentException("La direccion no debe estar vacia");
         }
         else{
             this.direccion = dir;
         }
+//        String cadTel = (String)tel; // esto no se puede hacer cast de primitivo a objeto
+        String cadTel = Long.toString(tel);
+        if(cadTel.length() == 10)
+        {
+            this.telefono = tel;
+        }
+        else
+        {
+            throw new IllegalArgumentException("El telefono debe de ser de 10 digitos");
+        }
+
+
+    }
+
+    @Override
+    public String toString() {
+
+        return "Nombre: " + nombre + ", " + "Direccion: " + direccion + ", " + "Telefono: "+ telefono;
     }
 }
