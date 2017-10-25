38f1f8068b643074a33f4ed4e955901ac0c4853f
israel <israel.vergara102824@gmail.com>
2017-10-25 11:04:59 -0500

    se agrega validacion de telefono y se agregan contactos y se imprimen

--- src/AplicacionTerminal.java
+++ src/AplicacionTerminal.java
@@ -1,7 +1,15 @@
 public class AplicacionTerminal
 {
     public static void main(String[] args) {
-        Contacto c1 = new Contacto("Israel", "mi casa", 123457890 );
+        Contacto c1 = new Contacto("Israel", "Mi casa", 1234578900 );
+        Contacto c2 = new Contacto("Mario", "la casa de el", 1234578900L );
+        Contacto c3 = new Contacto("alejadro", "mx", 1234578900L );
+
+        Agenda agenda = new Agenda();
+        agenda.agregaContacto(c1);
+        agenda.agregaContacto(c2);
+        agenda.agregaContacto(c3);
+        agenda.imprimeTodo();
 
     }
 }
