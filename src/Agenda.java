1f526a643976042cf29a5ac55b5f5c411e4a65f8
israel <israel.vergara102824@gmail.com>
2017-10-25 11:13:54 -0500

    se agrega validacion de telefono y se agregan contactos estos se imprimen

--- src/Agenda.java
+++ src/Agenda.java
@@ -3,4 +3,34 @@ import java.util.LinkedList;
 public class Agenda
 {
     private LinkedList<Contacto> contactos;
+
+    public Agenda()
+    {
+        contactos = new LinkedList<Contacto>();//notacion diamante <> es valido
+    }
+
+    public void agregaContacto(Contacto nuevo)
+    {
+        contactos.add(nuevo);
+    }
+
+    public void imprimeTodo()
+    {
+        //for traddicional
+/*        for (int i=0; i < contactos.size(); i++)
+        {
+            System.out.println(contactos.get(i));
+        }
+*/
+        //for-each
+        //sintaxis:
+        //for(Tipo var : coleccion
+        //{
+        //acciones con la variable var
+        //}
+        for(Contacto c : contactos)
+        {
+            System.out.println(c);
+        }
+    }
 }
