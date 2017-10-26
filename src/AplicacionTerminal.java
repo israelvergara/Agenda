da2cc0260e07e01bab546c95bbf6293215171876
israel <israel.vergara102824@gmail.com>
2017-10-26 06:07:07 -0500

    se agrega menu para agregar e imprimir un contacto se agrega metodo agergarUsuario(); y metodo agregar(); y el metodo demo(); para ver que los metodos stacticos no pueden acceder a la variables de instancia por lo que debemos agregar en el main(); una variable de tipo  AplicaionTerminal y asi poder acceder a otro metodo que no es estatico

--- src/AplicacionTerminal.java
+++ src/AplicacionTerminal.java
@@ -1,15 +1,67 @@
+import java.util.Scanner;
+
 public class AplicacionTerminal
 {
-    public static void main(String[] args) {
+    private Agenda agenda;
+
+    public AplicacionTerminal() {
+        agenda = new Agenda();
+    }
+
+    public void demo()
+    {
         Contacto c1 = new Contacto("Israel", "Mi casa", 1234578900 );
         Contacto c2 = new Contacto("Mario", "la casa de el", 1234578900L );
         Contacto c3 = new Contacto("alejadro", "mx", 1234578900L );
 
-        Agenda agenda = new Agenda();
-        agenda.agregaContacto(c1);
-        agenda.agregaContacto(c2);
-        agenda.agregaContacto(c3);
-        agenda.imprimeTodo();
+        //Agenda agenda = new Agenda();
+        this.agenda.agregaContacto(c1);
+        this.agenda.agregaContacto(c2);
+        this.agenda.agregaContacto(c3);
+        this.agenda.imprimeTodo();
+    }
+
+    public void entradaUsuario() {
+        Scanner entrada = new Scanner(System.in);
+        String opcion = "";
+
+        do {
+            try {
+                System.out.println("Opciones agregar - imprimir - terminar");
+                opcion = entrada.nextLine();
+                switch (opcion) {
+                    case "agregar":
+                        agregar(entrada);
+                        break;
+                    case "imprimir":
+                        agenda.imprimeTodo();
+                        break;
+                }
+            }
+            catch(IllegalArgumentException ex)
+            {
+                System.out.print(ex.getMessage());
+                opcion = "";
+            }
+        } while (opcion != "terminar");
+    }
+
+    private void agregar(Scanner entrada) {
+        System.out.println("Agregar un contacto nuevo");
+        System.out.print("Nombre: ");
+        String nombre = entrada.nextLine();
+        System.out.print("Direccion: ");
+        String direccion = entrada.nextLine();
+        System.out.print("Telefono: ");
+        Long tel = entrada.nextLong();
+        Contacto nuevo = new Contacto (nombre, direccion, tel);
+        agenda.agregaContacto(nuevo);
+
+    }
+
+    public static void main(String[] args) {
+        AplicacionTerminal aplicacion = new AplicacionTerminal();
+        aplicacion.entradaUsuario();
 
     }
 }
